f81f59cc8231552b96f5742b2a54042c053f4f17
Mizunashi Mana <mizunashi-mana@noreply.git>
2021-10-31 15:48:10 +0900

    Add syntax modules

---
+++ package/ptera/src/Language/Parser/Ptera/Syntax/Grammar.hs
@@ -0,0 +1,95 @@
+module Language.Parser.Ptera.Syntax.Grammar where
+
+import Language.Parser.Ptera.Prelude
+
+import qualified Data.EnumMap.Strict as EnumMap
+import qualified Language.Parser.Ptera.Syntax.SafeRule as SafeRule
+
+
+type GrammarT s n t e f = StateT (Context s n t e f)
+
+data Context s n t e f = Context
+    {
+        ctxStarts :: EnumMap.EnumMap s n,
+        ctxRules :: EnumMap.EnumMap n (RuleWrapper n t e f)
+    }
+
+fixed :: Monad m => GrammarT s n t e f m () -> m (FixedGrammar s n t e f)
+fixed builder = execStateT builder initialCtx where
+    initialCtx = Context
+        {
+            ctxStarts = EnumMap.empty,
+            ctxRules = EnumMap.empty
+        }
+
+data FixedGrammar s n t e f = FixedGrammar
+    {
+        ctxStarts :: EnumMap.EnumMap s n,
+        ctxRules :: EnumMap.EnumMap n (RuleWrapper n t e f)
+    }
+
+data RuleWrapper n t e f where
+    RuleWrapper :: SafeRule.Rule n t e f r -> RuleWrapper n t e f
+
+initialT :: Enum s => Monad m => s -> n -> GrammarT s n t e f m ()
+initialT s v = modify' \ctx -> ctx
+    {
+        ctxStarts = EnumMap.insert s v
+            do ctxStarts ctx
+    }
+
+ruleT :: Enum n => Monad m
+    => n -> [GrammarT s n t e f m (SafeRule.Alt n t e f r)]
+    -> GrammarT s n t e f m (SafeRule.Unit n t e r)
+ruleT v mes = do
+    rules <- ctxRules <$> get
+    case EnumMap.lookup v rules of
+        Just _ ->
+            pure ()
+        Nothing -> do
+            es <- sequence mes
+            let r = RuleWrapper do SafeRule.Rule v es
+            modify' \ctx -> ctx
+                {
+                    ctxRules = EnumMap.insert v r
+                        do ctxRules ctx
+                }
+    pure do SafeRule.UnitVar v
+
+(<^>) :: Monad m
+    => GrammarT s n t e f m (SafeRule.Unit n t e u)
+    -> GrammarT s n t e f m (SafeRule.Expr n t e us)
+    -> GrammarT s n t e f m (SafeRule.Expr n t e (u ': us))
+mu <^> me = do
+    u <- mu
+    e <- me
+    pure do u SafeRule.:^ e
+
+infixr 5 <^>
+
+(<^^>) :: Monad m
+    => GrammarT s n t e f m (SafeRule.Unit n t e u1)
+    -> GrammarT s n t e f m (SafeRule.Unit n t e u2)
+    -> GrammarT s n t e f m (SafeRule.Expr n t e '[u1, u2])
+mu1 <^^> mu2 = do
+    u1 <- mu1
+    u2 <- mu2
+    pure do u1 SafeRule.:^ u2 SafeRule.:^ SafeRule.Eps
+
+infixr 5 <^^>
+
+(<:>) :: Monad m
+    => GrammarT s n t e f m (SafeRule.Expr n t e us)
+    -> f us r
+    -> GrammarT s n t e f m (SafeRule.Alt n t e f r)
+me <:> act = do
+    e <- me
+    pure do SafeRule.Alt e act
+
+infixr 8 <:>
+
+epsT :: Monad m => GrammarT s n t e f m (SafeRule.Expr n t e '[])
+epsT = pure SafeRule.Eps
+
+tokenT :: Monad m => t -> GrammarT s n t e f m (SafeRule.Unit n t e e)
+tokenT t = pure do SafeRule.UnitToken t
